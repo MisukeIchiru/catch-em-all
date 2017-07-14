6de23f0a919ba6a98ef3118c6b98174dd7060f6c
Niklas Debbrecht <niklas@debbrecht.com>
2017-07-14 15:09:15 +0200

    links for tabs are working v2

---
+++ catch-em-all/src/pages/karte/karte.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-karte',
+  templateUrl: 'karte.html'
+})
+export class KartePage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
